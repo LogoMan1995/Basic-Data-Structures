1ec967a9453c1ca82bbd27466ae5970232e36c50
LogoMan1995 <david.grigorian777@gmail.com>
2024-04-04 16:21:34 +0400

    Access an Array's Contents Using Bracket Notation

---
+++ ex02.js
@@ -0,0 +1,6 @@
+// 2. Чтобы выполнить это задание, установите вторую позицию (индекс 1) myArray на все, что захотите, кроме буквы b.
+
+let myArray = ["a", "b", "c", "d"];
+myArray[1]="I ate the letter b";
+console.log(myArray);
+
